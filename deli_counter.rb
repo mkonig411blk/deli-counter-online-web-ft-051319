b9fb20c26c199ed313040ae6543a555f8c1c6abc
Margot Konig <margot.konig@blackrock.com>
2019-06-23 01:02:30 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -10,8 +10,8 @@ katz_deli = []
         number = index + 1
         new_string  = "#{number}. #{name}, "
         string << new_string 
+        puts string
       end
-     puts string.slice(0..-3) 
     
   else 
   puts "The line is currently empty."
